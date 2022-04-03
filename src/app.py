0433318a39588a5ab81e3b34c7cf68a2d617fd33
Felipe Arruda <felipearrudajf@gmail.com>
2022-04-03 18:36:42 -0300

    heroku

--- src/app.py
+++ src/app.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Bem vindo!</p>"
 
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
