6e64ee919c955fe732fd1e51b132a80105a019c0
Felipe Arruda <felipearrudajf@gmail.com>
2022-04-03 19:54:09 -0300

    wip

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from mysql.connector import errorcode
+from twitchio.ext.commands.errors import CommandNotFound
 import os
 import mysql.connector
-from mysql.connector import errorcode
-
-
-
+import datetime
 
 app = Flask(__name__)
 
@@ -40,6 +39,42 @@ def hello_world():
         cursor.execute(add_event_message, data_event_message)
         cnx.commit()
         cursor.close()
+
+    class Bot(commands.Bot):
+
+        def __init__(self):
+            # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+            # prefix can be a callable, which returns a list of strings or a string...
+            # initial_channels can also be a callable which returns a list of strings...
+            super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?',
+                             initial_channels=['gaules', 'yoda', 'summit1g', 'casimito', 'mch_agg', 'ale_apoka',
+                                               'hasanabi', 'mizkif', 'elspreen'])
+            # super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?', initial_channels=['sudo4root'])
+
+        async def event_ready(self):
+            # Notify us when everything is ready!
+            # We are logged in and ready to chat and use commands...
+            print(f'Logged in as | {self.nick}')
+            print(f'User id is | {self.user_id}')
+
+        async def event_message(self, message, initial_channels=None):
+            # Messages with echo set to True are messages sent by the bot...
+            # For now we just want to ignore them...
+            if message.echo:
+                return
+
+            # Print the contents of our message to console...
+            # print(message.channel.name + " - " +message.author.name + " - " + message.content + " - " + message.timestamp.strftime("%Y-%m-%d %H:%M:%S"))
+
+            data_event_message = (message.author.name, message.channel.name, message.content,
+                                  message.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+                                  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            pushEventMessage(data_event_message)
+
+            # Since we have commands and are overriding the default `event_message`
+            # We must let the bot know we want to handle and invoke our commands...
+            if not CommandNotFound:
+                await self.handle_commands(message)
 
 
 if __name__ == "__main__":
