3434eb217741d6903e00a1ff29c51aabd29b0648
Felipe Arruda <felipearrudajf@gmail.com>
2022-04-03 19:45:55 -0300

    wip

--- app.py
+++ app.py
@@ -12,21 +12,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Mysql!</p>"
 
-    try:
-        cnx = mysql.connector.connect(user='felipearruda', password='felipejf34',
-                                      host='mysql-twitch.cf7idmglqggw.sa-east-1.rds.amazonaws.com',
-                                      database='twitch')
-        print("Conectado")
-
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
