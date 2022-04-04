abe18d25a059df7032099f61d7f4449545ae1ad7
Felipe Arruda <felipearrudajf@gmail.com>
2022-04-04 14:44:41 -0300

    wip

--- worker.py
+++ worker.py
@@ -53,12 +53,12 @@ class Bot(commands.Bot):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?',
-                         initial_channels=['gaules', 'yoda', 'summit1g', 'casimito', 'mch_agg', 'ale_apoka',
-                                           'hasanabi', 'mizkif', 'elspreen', 'rubius', 'kamet0', 'xqcow'])
-        # super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?', initial_channels=['sudo4root'])
         # super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?',
-        #                  initial_channels=['gaules', 'casimito', 'mch_agg', 'ale_apoka'])
+        #                  initial_channels=['gaules', 'yoda', 'summit1g', 'casimito', 'mch_agg', 'ale_apoka',
+        #                                    'hasanabi', 'mizkif', 'elspreen', 'rubius', 'kamet0', 'xqcow'])
+        # super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?', initial_channels=['sudo4root'])
+        super().__init__(token='67qpusn45w05rniu5t5gj83c0a1p9d', prefix='?',
+                         initial_channels=['gaules', 'casimito', 'mch_agg', 'ale_apoka'])
 
     async def event_ready(self):
         # Notify us when everything is ready!
