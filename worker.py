4e794fe02aed6bc9b3f3f879fb9749c6eff7e455
Felipe Arruda <felipearrudajf@gmail.com>
2022-04-03 22:19:36 -0300

    wip

--- worker.py
+++ worker.py
@@ -80,8 +80,7 @@ class Bot(commands.Bot):
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
         if not CommandNotFound:
-            pass
-            # await self.handle_commands(message)
+            await self.handle_commands(message)
 
 
 
