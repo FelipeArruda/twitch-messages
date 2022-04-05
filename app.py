b5cf867f6058606c69934dd707a292533dfad747
Felipe Arruda <felipearrudajf@gmail.com>
2022-04-04 22:31:13 -0300

    add streamlit

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-# from flask import Flask
-# import os
 import streamlit as st
 
-# app = Flask(__name__)
+st.set_page_config("Twitch - Análise de dado")
 
-
-
-# @app.route("/")
-# def hello_world():
-#     return "AWS 25"
 def main():
+
     page = st.sidebar.selectbox("Escolha uma opção:", ['Opção 1', 'Opção 2', 'Opção 3'])
-    st.header("Twicth - Análise de dados")
+    st.header("Twitch - Análise de dadosss")
     st.write('Esta é uma página de teste.')
 
 if __name__ == "__main__":
     main()
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
